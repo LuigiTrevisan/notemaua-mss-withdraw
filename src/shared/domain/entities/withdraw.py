49f8765ad755273f2678b0c1ed400d1bc0de0c3a
Luigi Trevisan <checkzin123@gmail.com>
2023-04-26 08:42:12 -0300

    withdraw + tests

---
+++ src/shared/domain/entities/withdraw.py
@@ -0,0 +1,46 @@
+import abc
+from src.shared.domain.entities.user import User
+
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.domain.entities.notebook import Notebook
+
+
+class Withdraw(abc.ABC):
+    num_serie: str
+    email: str
+    withdraw_time: int
+    return_time: int
+    
+    def __init__(self, num_serie: str, email: str, withdraw_time: int, return_time: int == None):
+        if not Notebook.validate_num_serie(num_serie):
+            raise EntityError("num_serie")
+        self.num_serie = num_serie
+        
+        if not User.validate_email(email):
+            raise EntityError("email")
+        self.email = email
+        
+        if not Withdraw.validate_time(withdraw_time):
+            raise EntityError("withdraw_time")
+        self.withdraw_time = withdraw_time
+        
+        if return_time is not None:
+            if not Withdraw.validate_time(return_time):
+                raise EntityError("return_time")
+            if return_time < withdraw_time:
+                raise EntityError("return_time")
+            self.return_time = return_time
+        else:
+            self.return_time = None
+        
+        
+        
+    @staticmethod
+    def validate_time(time) -> bool:
+        if time == None:
+            return False
+        if type(time) != int:
+            return False
+        if time < 1641006000000:
+            return False
+        return True
---
+++ tests/shared/domain/entities/test_withdraw.py
@@ -0,0 +1,74 @@
+import pytest
+
+from src.shared.domain.entities.withdraw import Withdraw
+from src.shared.helpers.errors.domain_errors import EntityError
+
+class Test_Withdraw:
+    
+    def test_withdraw(self):
+        withdraw = Withdraw(
+            num_serie="34756",
+            email="22.01102-0@maua.br",
+            withdraw_time=1682508271948,
+            return_time=None
+        )
+        
+        assert type(withdraw) == Withdraw
+        assert withdraw.num_serie == "34756"
+        assert withdraw.email == "22.01102-0@maua.br"
+        assert withdraw.withdraw_time == 1682508271948
+        assert withdraw.return_time == None
+        
+    def test_withdraw_withdraw_time_is_none(self):
+        with pytest.raises(EntityError):
+            withdraw = Withdraw(
+            num_serie="34756",
+            email="22.01102-0@maua.br",
+            withdraw_time=None,
+            return_time=None
+        )
+            
+    def test_withdraw_withdraw_time_not_int(self):
+        with pytest.raises(EntityError):
+            withdraw = Withdraw(
+            num_serie="34756",
+            email="22.01102-0@maua.br",
+            withdraw_time="1682508271948",
+            return_time=None
+        )
+            
+    def test_withdraw_withdraw_time_past(self):
+        with pytest.raises(EntityError):
+            withdraw = Withdraw(
+            num_serie="34756",
+            email="22.01102-0@maua.br",
+            withdraw_time=1071972000000,
+            return_time=None
+        )
+            
+    def test_withdraw_return_time_not_int(self):
+        with pytest.raises(EntityError):
+            withdraw = Withdraw(
+            num_serie="34756",
+            email="22.01102-0@maua.br",
+            withdraw_time=1682508271948,
+            return_time="1682509207926"
+        )
+            
+    def test_withdraw_return_time_past(self):
+        with pytest.raises(EntityError):
+            withdraw = Withdraw(
+            num_serie="34756",
+            email="22.01102-0@maua.br",
+            withdraw_time=1682508271948,
+            return_time=1071972000000
+        )
+            
+    def test_withdraw_return_time_smaller_than_withdraw(self):
+        with pytest.raises(EntityError):
+            withdraw = Withdraw(
+            num_serie="34756",
+            email="22.01102-0@maua.br",
+            withdraw_time=1682509207926,
+            return_time=1682508271948
+        )
