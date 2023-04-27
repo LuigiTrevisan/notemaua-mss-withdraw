3fd22f98b5bca463058366babf47de89551942fe
LUIGI GUIMARAES TREVISAN <22.01102-0@maua.br>
2023-04-27 14:34:04 -0300

    #43 - Criação dos métodos na interface
    Signed-off-by: LUIGI GUIMARAES TREVISAN <22.01102-0@maua.br>

--- src/shared/domain/repositories/withdraw_repository_interface.py
+++ src/shared/domain/repositories/withdraw_repository_interface.py
@@ -1,4 +1,47 @@
 from abc import ABC, abstractmethod
 
 class IWithdrawRepository(ABC):
-    pass
+    
+    def get_user_by_email(self, email):
+        '''
+        If user with email exists, return User
+        else return None
+        '''
+        pass
+    
+    def get_notebook(self, num_serie):
+        '''
+        If notebook with num_serie exists, return Notebook
+        else return None
+        '''
+        pass
+    
+    def get_all_withdraws(self):
+        '''
+        Return all withdraws
+        '''
+        pass
+    
+    def get_withdraw_by_num_serie(self, num_serie):
+        '''
+        If withdraw with num_serie exists, return Withdraw
+        else return None
+        '''
+        pass
+    
+    def create_withdraw(self, num_serie, email):
+        '''
+        If notebook exists and isActive == False and user exists, create withdraw and return it
+        set Notebook.isActive to True
+        set withdraw_time to current time
+        '''
+        pass
+    
+    def finish_withdraw(self, num_serie):
+        '''
+        If withdraw with num_serie exists, finish withdraw and return it
+        set Notebook.isActive to False
+        set Withdraw.return_time to current time
+        get user and notebook and send them to withdraw history
+        '''
+        pass
