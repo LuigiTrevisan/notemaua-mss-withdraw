328748715cefc548c5454ed828af5074d95409f3
Bidetti <rafael.bidetti@gmail.com>
2023-05-19 10:15:16 -0300

    usecase

---
+++ src/modules/get_all_notebooks/app/get_all_notebook_usecase.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from src.shared.domain.entities.notebook import Notebook
+from src.shared.domain.repositories.withdraw_repository_interface import IWithdrawRepository
+
+class GetAllNotebooksUsecase:
+    
+    def __init__(self, repo: IWithdrawRepository):
+        self.repo = repo
+        
+    def __call__(self) -> List[Notebook]:
+        notebooks = self.repo.get_all_notebooks()
+        active_withdraws = []
+        for notebook, withdraws in notebooks:
+            if notebook.isActive == True:
+                for withdraw in withdraws:
+                    if withdraw.finish_time == None:
+                        active_withdraws.append((notebook, [withdraw]))
+            else:
+                active_withdraws.append((notebook, None))
+        return active_withdraws
---
+++ tests/modules/get_all_notebooks/app/test_get_all_notebooks_usecase.py
@@ -0,0 +1,12 @@
+from src.modules.get_all_notebooks.app.get_all_notebook_usecase import GetAllNotebooksUsecase
+import pytest
+
+from src.shared.infra.repositories.withdraw_repository_mock import WithdrawRepositoryMock
+
+class Test_GetAllNotebooksUsecase:
+    
+    def test_get_all_notebooks_usecase(self):
+        repo = WithdrawRepositoryMock()
+        usecase = GetAllNotebooksUsecase(repo=repo)
+        notebooks = usecase()
+        assert True
