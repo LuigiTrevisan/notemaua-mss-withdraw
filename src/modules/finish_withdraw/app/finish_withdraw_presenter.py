cb37b82dc7fbb9bd0ccd93f113a814d5c40e36f4
Diimy <100159300+Diimy@users.noreply.github.com>
2023-05-25 22:19:29 -0300

    presenter+tests

---
+++ src/modules/finish_withdraw/app/finish_withdraw_presenter.py
@@ -0,0 +1,15 @@
+from .finish_withdraw_controller import FinishWithdrawController
+from .finish_withdraw_usecase import FinishWithdrawUsecase
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+repo = Environments.get_withdraw_repo()()
+usecase = FinishWithdrawUsecase(repo=repo)
+controller = FinishWithdrawController(usecase=usecase)
+
+def lambda_handler(event, context):
+    httpRequest = LambdaHttpRequest(data=event)
+    response = controller(request=httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+
+    return httpResponse.toDict()
