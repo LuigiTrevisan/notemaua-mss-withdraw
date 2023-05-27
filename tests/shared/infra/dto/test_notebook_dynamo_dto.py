143a6a75e9db618c78720c0652ff1c1cca9b6932
Luigi Trevisan <checkzin123@gmail.com>
2023-05-27 10:29:57 -0300

    test notebook dto fix

--- tests/shared/infra/dto/test_notebook_dynamo_dto.py
+++ tests/shared/infra/dto/test_notebook_dynamo_dto.py
@@ -30,17 +30,17 @@ class Test_NotebookDynamoDTO:
         
     def test_notebook_dynamo_dto_from_entity_to_dynamo(self):
         notebook = Notebook(num_serie="34036", isActive=True)
-        notebook_dto = NotebookDynamoDTO.from_entity(notebook)
-        assert notebook_dto.to_dynamo() == {
+        notebook_dto = NotebookDynamoDTO.from_entity(notebook).to_dynamo()
+        assert notebook_dto == {
             "entity" : "notebook",
             "num_serie": "34036",
             "isActive": True
         }
         
     def test_notebook_dynamo_dto_from_dynamo_to_entity(self):
-        notebook_dto = NotebookDynamoDTO.from_dynamo({
+        notebook = NotebookDynamoDTO.from_dynamo({
             "entity" : "notebook",
             "num_serie": "34036",
             "isActive": True
-        })
-        assert notebook_dto.to_entity() == Notebook(num_serie="34036", isActive=True)
+        }).to_entity()
+        assert notebook == Notebook(num_serie="34036", isActive=True)
